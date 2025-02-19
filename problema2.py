e2966a3fdbfde40f231542a02553beb137df1e33
ezequielvh <ezequiel.velasque@gmail.com>
2025-02-18 23:23:40 -0500

    Update problema2.py

--- problema2.py
+++ problema2.py
@@ -1,8 +1,19 @@
 
-def main():
-    S1  = input()
-    S2  = input()
+from collections import Counter
+import sys
 
+def son_permutaciones(s1, s2):
+    if len(s1) != len(s2):
+        return "NO"
+    return "SI" if Counter(s1) == Counter(s2) else "NO"
+
+def main():
+    s1 = input().strip()
+    s2 = input().strip()
+    print(son_permutaciones(s1, s2))
+    
+son_permutaciones('aroma', 'romaa')
+son_permutaciones('arroz', 'lentejas')
 
 if __name__ == '__main__':
     main()
