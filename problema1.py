fcf6f43d1faca93dffc402d231fef50466c98437
ezequielvh <ezequiel.velasque@gmail.com>
2025-02-18 23:03:54 -0500

    Update problema1.py

--- problema1.py
+++ problema1.py
@@ -1,10 +1,14 @@
 
+def es_palindromo(s):
+    return "Es palindrome" if s == s[::-1] else "No es palindrome"
 
 def main():
-    S       = input()
-    
+    S = input().strip()
+    print(es_palindromo(S))
 
 if __name__ == '__main__':
     main()
-
+    
+es_palindromo(reconocer)
+es_palindromo(correr)
 
